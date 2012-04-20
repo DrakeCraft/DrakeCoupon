e72313e3b4f354979bf96a703f32b04f1bea67bb
Nijokkin <AlexButler2012@gmail.com>
2012-04-20 16:38:14 +1000

    added services and refactored code

--- src/main/java/com/drakecoupon/DataPool.java
+++ src/main/java/com/drakecoupon/DataPool.java
@@ -1,32 +1,69 @@
 package com.drakecoupon;
 
-import java.util.logging.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import net.milkbowl.vault.economy.Economy;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class DataPool {
 
 	private DrakeCoupon dc;
-	public Economy economy = null;
+	private File couponFile;
+	private YamlConfiguration couponConfig;
 	
-	public static boolean useEconomy = true;
+	private Economy economy;
+	
+	public final ArrayList<String> ServiceList = new ArrayList<String>();
+	private ArrayList<String> EffectiveServiceList = new ArrayList<String>();
 	
 	public DataPool(DrakeCoupon dc)
 	{
 		this.dc = dc;
 		
-		if(setupEconomy())
-		{
-			Logger.getLogger("[DrakeCoupon] Economy was found!");
-		} else {
-			useEconomy = false;
-			Logger.getLogger("[DrakeCoupon] Economy was not found! Economic features have been disabled!");
-		}
+		addDefaults();
+		setupConfiguration();
+		
+		setupEconomy();
+		
+		ServiceList.add("Economy");
 	}
 	
-	private boolean setupEconomy()
+	private void addDefaults()
+	{
+		dc.getConfig().addDefault("options.files.couponfile", "coupons.yml");
+		dc.getConfig().options().copyDefaults(true);
+		dc.saveConfig();
+	}
+	
+	private void setupConfiguration()
+	{
+		couponFile = new File(dc.getDataFolder(), getCouponFileName());
+		
+		if(!couponFile.exists())
+		{
+			try {
+				couponFile.createNewFile();
+				couponConfig = YamlConfiguration.loadConfiguration(couponFile);
+			} catch (IOException e) {}
+		} else {
+			couponConfig = YamlConfiguration.loadConfiguration(couponFile);
+		}
+		
+		couponConfig.addDefault("18876", "AdminCoupon;Never");
+		couponConfig.options().header("Coupon codes storage file. Editing may cause unintended behaviour!");
+		couponConfig.options().copyDefaults(true);
+		couponConfig.options().copyHeader(true);
+		
+		try {
+			couponConfig.save(couponFile);
+		} catch (IOException e) {}
+	}
+	
+	private void setupEconomy()
 	{
 		RegisteredServiceProvider<Economy> economyProvider = dc.getServer().getServicesManager().getRegistration(Economy.class);
 		
@@ -35,6 +72,24 @@ public class DataPool {
 			economy = economyProvider.getProvider();
 		}
 		
-		return (economy != null);
+		if(economy != null)
+		{
+			EffectiveServiceList.add("Economy");
+		}
+	}
+	
+	public ArrayList<String> enabledServices()
+	{
+		return EffectiveServiceList;
+	}
+	
+	public String getCouponFileName()
+	{
+		return dc.getConfig().getString("options.files.couponfile");
+	}
+	
+	public String getCoupon(String couponId)
+	{
+		return couponConfig.getString(couponId);
 	}
 }
--- src/main/java/com/drakecoupon/DrakeCoupon.java
+++ src/main/java/com/drakecoupon/DrakeCoupon.java
@@ -2,29 +2,33 @@ package com.drakecoupon;
 
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DrakeCoupon extends JavaPlugin {
 	
-	private DrakeConfig config;
 	private DataPool dp;
 	
 	public void onEnable()
 	{
-		config = new DrakeConfig(this);
 		dp = new DataPool(this);
 		
-		Logger.getLogger("Minecraft").info(config.getCoupon("12345"));
-		
-		getConfig().addDefault("options.files.couponfile", "coupons.yml");
-		config.saveConfiguration();
-		
-		if(DataPool.useEconomy)
-		dp.economy.bankBalance("TheRandomGuy10");
+		if(dp.enabledServices() == dp.ServiceList)
+		{
+			Logger.getLogger("Minecraft").info(ChatColor.GREEN + "[DrakeCoupon] Services have started! Running normally!");
+		} else {
+			Logger.getLogger("Minecraft").info(ChatColor.RED + "[DrakeCoupon] One or more of the services are currently not running!");
+			Logger.getLogger("Minecraft").info(ChatColor.RED + "[DrakeCoupon] Currently enabled services:");
+			
+			for(int i = 0; i < dp.enabledServices().size(); i++)
+			{
+				Logger.getLogger("Minecraft").info(ChatColor.RED + dp.enabledServices().get(i));
+			}
+		}
 	}
 	
 	public void onDisable()
 	{
-		config.saveConfiguration();
+		Logger.getLogger("Minecraft").info("[DrakeCoupon] Shutting down services!");
 	}
 }
